cc4a619b4f7c5706a13881a107770d810f2dc258
federico <federicopavone01@gmail.com>
2017-12-14 13:00:28 +0100

    update for sparse matrix

--- solver.cpp
+++ solver.cpp
@@ -1,34 +1,45 @@
 #include "solver.hpp"
 #include <Eigen/Core>
-#include <Eigen/Dense>
-#include <Eigen/QR>
-// #include <Eigen/IterativeLinearSolvers>
+#include<Eigen/SparseLU>
+#include<Eigen/SparseQR>
+#include <Eigen/IterativeLinearSolvers>
 using namespace Eigen;
 
+// NOTE QUANDO USARE QR O LU????
 
 void Solver::setup(){
-  if(Method == MethodType::DEFAULT){
-    if(A.rows() > NTreshold)
-      Method = MethodType::ITERATIVE;
-    else
-      Method = MethodType::DIRECT;
-  }
+  if(Inv == IsInvertible::T && Method == MethodType::DIRECT) Option = SolverOption::LU;
   if(Inv == IsInvertible::F && Method == MethodType::DIRECT) Option = SolverOption::QR;
   else Option = SolverOption::LSCG;
 }
 
 //gestire errori
+// NOTE È UNA BUONA IDEA CREARE I SOLVER DENTRO UNA FUNZIONE? ALLA FINE VENGONO DISTRUTTI
+
 VectorXd Solver::solve(const VectorXd & b){
 switch(Option){
+  case SolverOption::LU: {
+    SparseLU<SparseMatrix<scalar, ColMajor>, COLAMDOrdering<Index> >   solver;
+    // Compute the ordering permutation vector from the structural pattern of A
+    solver.analyzePattern(A);
+    // Compute the numerical factorization
+    solver.factorize(A);
+    //Use the factors to solve the linear system
+    Sol = solver.solve(b);
+  }
   case SolverOption::QR: {
-    ColPivHouseholderQR<MatrixXd> dec(A);   // È buona cosa creare il  solver dentro una funzione?
-                                            // terminata la funzione viene distrutto!
-    Sol = dec.solve(b);
+    SparseLU<SparseMatrix<scalar, ColMajor>, COLAMDOrdering<Index> >   solver;
+    // Compute the ordering permutation vector from the structural pattern of A
+    solver.analyzePattern(A);
+    // Compute the numerical factorization
+    solver.factorize(A);
+    //Use the factors to solve the linear system
+    Sol = solver.solve(b);
   }
   case SolverOption::LSCG: {
-    // LeastSquaresConjugateGradient<MatrixXd> lscg;
-    // lscg.compute(A);
-    // Sol = lscg.solve(b);
+    LeastSquaresConjugateGradient<SparseMatrix<double> > lscg;
+    lscg.compute(A);
+    Sol = lscg.solve(b);
   }
 }
 };
--- solver.hpp
+++ solver.hpp
@@ -1,20 +1,21 @@
 #ifndef __MY__SOLVER_
 #define __MY__SOLVER_
 #include <Eigen/Core>
-#include <Eigen/Dense>
-#include <Eigen/QR>
-// #include <Eigen/IterativeLinearSolvers>
+// #include<Eigen/SparseLU>
+// #include<Eigen/SparseQR>
+// #include<Eigen/IterativeLinearSolvers>
 
 using namespace Eigen;
-// CONTROLLARE TUTTE LE & e I CONST!!!!
+// NOTE CONTROLLARE TUTTE LE & e I CONST!!!!
 
 // Solving linear system of the type Ax = b
-enum class MethodType {DIRECT, ITERATIVE, DEFAULT};
+enum class MethodType {DIRECT, ITERATIVE};
 enum class IsInvertible {T,F};
 enum class SolverOption{QR,LU,LSCG};
 // Se il metodo è automatico, guardare dimensione n e decidere se
 // farlo iterativo o diretto
 
+// NOTE AGGIUSTARE: COME PASSARE UN OGGETTO EIGEN PER REFERENCE
 class Solver{
 private:
   const MatrixXd & A;
@@ -24,7 +25,7 @@ private:
   static const int NTreshold = 1000; // treshold tra metodo diretto e iterativo
   SolverOption Option;
 public:
-  Solver(const MatrixXd & AA, MethodType MethodInput = MethodType::DEFAULT,
+  Solver(const MatrixXd & AA, MethodType MethodInput = MethodType::DIRECT,
     IsInvertible InvInput = IsInvertible::F):
     A(AA), Sol(AA.rows()), Method(MethodInput), Inv(InvInput) {}; // CONTROLLARE #RIGHE = #COLONNE?
   // altri costruttori
@@ -32,7 +33,8 @@ public:
   // e Inv (ed eventualmente il condizionamento)
   // Si potrebbe anche aggiungere caso se è simmetrica o definita/semidefinita
   // setup() dovrebbe funzionare come una factory, per adesso faccio classe enum dei solver
-  VectorXd solve(const VectorXd & b);
+  void solve(const VectorXd & b);
+  inline VectorXd getsolution(){return Sol;};
 };
 
 #endif
