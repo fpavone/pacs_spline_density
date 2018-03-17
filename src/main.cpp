e6a5de2a0f4f9a10e92de9dd4c4eec7bc546f0ee
federico <federicopavone01@gmail.com>
2018-03-17 17:11:14 +0100

    corrected value of n, now main works

--- src/main.cpp
+++ src/main.cpp
@@ -10,12 +10,14 @@ int main() {
     std::vector<double> const cp{-0.5, -0.2, 0.2, 0.5};
     std::vector<double> const weights{1.0, 1.0, 1.0, 1.0, 1.0};
 
-    const int n = 3;//cp.size()-1; //Number of control points - 1
+    const int n = 4;//corretto: cp.size()     OLD: cp.size()-1; //Number of control points - 1
     const int k = 2; //Spline degree
     const int g = 2;//knots.size()-2;  //number of knots excluding the end points
     const int G = g + k + 1;
 
     Density<k, n, G> MyDensity(1.0, knots, cp, weights);
     MyDensity.print_all();
+    MyDensity.solve();
+    MyDensity.print_sol();
     return 0;
 }
