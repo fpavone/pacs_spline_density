a750a831f4e49025c1ea0cb9476a16e93a531500
Gianluca <gianlupalla.2@hotmail.it>
2018-03-21 15:54:05 +0100

    mother class: working..

--- src/classData.hpp
+++ src/classData.hpp
@@ -7,11 +7,16 @@
 #include <iostream>
 
 // NOTE: suppose data are stored as vector of vectors named data
+// NOTE: data are assumed to be already without extreme observations
+
 
 class Mother {
 
   std::vector<std::vector<double>> data;
+  std::vector<std::vector<double>> prop_data;
   std::vector<std::vector<double>> transf_data;
+
+  unsigned int nclasses;
 
   unsigned int
   Sturge () const
@@ -44,9 +49,17 @@ class Mother {
   }
 
   void
-  transformData ()
+  propData ()
   {
-    for(const auto& x:data)
+    std::vector<double> v(nclasses,0.0);
+    for(const auto& x:data){
+      for (const auto& v:x)
+        v[] ++;
+      std::transform(v.begin(), v.end(), v.begin(),
+               std::bind1st(std::divides<double>(),x.size()));
+      prop_data.push_back(v);
+    }
+
 
 
   }
