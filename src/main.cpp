79ede6ba29735b886bd1e3b66d2bf009c1889d4a
Gianluca Zeni <gianlupalla.2@hotmail.it>
2018-03-21 14:40:02 +0100

    mistake corrected in xcp and ycp example

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,8 @@ int main() {
 
     //1st ROW EXAMPLE
     std::vector<double> const knots{0.0, 30000, 70000, 110709}; //Knot sequence
-    std::vector<double> const xcp{0.587, 2.331, 2.154, 1.271, 0.331, −0.550, −1.437, −1.997, −2.690};
-    std::vector<double> const ycp{6574, 19591, 32608, 45625, 58641, 71658, 84675, 97692, 110709};
+    std::vector<double> const ycp{0.587, 2.331, 2.154, 1.271, 0.331, −0.550, −1.437, −1.997, −2.690};
+    std::vector<double> const xcp{6574, 19591, 32608, 45625, 58641, 71658, 84675, 97692, 110709};
     const int n = xcp.size(); //corretto: cp.size()     OLD: cp.size()-1; //Number of control points - 1
     const int k = 3; //Spline degree
     unsigned int l = 2;
