5662f05c0095ca79b13176f31f174864e8e5dd62
Unknown <gianlupalla.2@hotmail.it>
2018-03-21 15:08:43 +0100

    main '-' corrected

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ int main() {
     //1st ROW EXAMPLE
     std::vector<double> const knots{0.0, 30000, 70000, 110709}; //Knot sequence
     std::vector<double> const xcp{6574, 19591, 32608, 45625, 58641, 71658, 84675, 97692, 110709};
-    std::vector<double> const ycp{0.587, 2.331, 2.154, 1.271, 0.331, −0.550, −1.437, −1.997, −2.690};
+    std::vector<double> const ycp{0.587, 2.331, 2.154, 1.271, 0.331, -0.550, -1.437, -1.997, -2.690};
 
     unsigned int k = 3; //Spline degree
     unsigned int l = 2;
