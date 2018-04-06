f054c2d454d4228cf0064eabe0209765ec38ac67
federico <federicopavone01@gmail.com>
2018-04-06 15:13:41 +0200

    updating classData

--- src/classData.hpp
+++ src/classData.hpp
@@ -8,7 +8,9 @@
 #include <vector>
 #include <algorithm>
 #include <functional>
+#include <string>
 #include "ctzeros.hpp"
+#include "density_estimation.hpp"
 
 // NOTE: suppose data are stored as vector of vectors named data
 // NOTE: data are supposed not to be ordered (otherwise counting occurrences is easier)
@@ -23,123 +25,196 @@
 
 class Mother {
 
-    // std::vector<std::vector<double>> data;
-    // std::vector<std::vector<double>> prop_data;
-    std::vector<std::vector<double>> transf_data;
+private:
 
-    unsigned int k;  // Spline degree
-    unsigned int l;
-    double alpha;  // penalization parameter
-    bool knots_given;
+  unsigned int k;  // Spline degree
+  unsigned int l;
+  double alpha;  // penalization parameter
 
-    unsigned int nclasses;
-    double min, max;
-    std::vector<double> intervals;
+  std::vector<double> knots; // spline knots
+  std::vector<double> xcp;
 
-    Mother(unsigned int kk, unsigned int ll, double opt_param, bool given):
-      k(kk), l(ll), alpha(opt_param), knots_given(given);
+  // std::vector<std::vector<double>> data;
+  std::vector<std::vector<double>> prop_data; // count data with zero replacement
+  std::vector<std::vector<double>> transf_data; // clr-transformed data
 
-    unsigned int
-    set_nclasses () const
-    {
-      // Adopting Sturge Formula
-      double k=0.0;
-      for(const auto& x:data)
-        k += 1.0 + ceil(log2(x.size()));
-      return (int) k/data.size();
+  Density dens;
+
+  // NOTE: generalization for different data input
+  unsigned int nclasses;
+  double min, max;
+  std::vector<double> intervals;
+
+public:
+  Mother(unsigned int kk, unsigned int ll, double opt_param):
+    k(kk), l(ll), alpha(opt_param) {};
+
+  void createKnots()
+  {
+    std::cout << "ATTENTO NODI NON CREATI: DA FINIRE.." << std::endl;
+  }
+
+  void readData(const std::string & fileD)
+  {
+    std::string line;
+
+    std::ifstream file_stream(fileD, std::ios_base::in);
+    if (!file_stream) {
+      std::cout << "Could not open file " << fileD << std::endl;
     }
 
-    void
-    set_intervals()
+    // Read xcp
+    getline(file_stream, line, '\n');
+    std::stringstream line_stream(line);
+    std::istream_iterator<double> start(line_stream), end;
+    std::vector<double> numbers(start, end);
+    for (const auto x:numbers)
+      xcp.push_back(x);
+
+    // Read prop_data
+    while (getline(file_stream, line, '\n')) // Split up each line of the file.
     {
-      double l = (max-min)/nclasses;
-      unsigned int i = 0;
-      while (i < nclasses+1)
-        intervals.push_back(min + (i++)*l);
+      std::stringstream line_stream(line);
+      // Get a vector of numbers directly from a stream iterator.
+      std::istream_iterator<double> start(line_stream), end;
+      std::vector<double> numbers(start, end);
+      // Taking account zero values replacement with BM
+      prop_data.push_back(coda::BM(numbers));
     }
 
+  }
 
+  void readData(const std::string & fileD, const std::string & fileK)
+  {
+    readData(fileD);
 
-    bool
-    loadData (const std::string& filepath)
+    // Read knots
+    std::ifstream file_stream(fileK, std::ios_base::in);
+    if (!file_stream)
     {
-      std::ifstream file_stream(filepath, std::ios_base::in);
-      if (!file_stream) {
-        std::cout << "Could not open file " << filepath << std::endl;
-        return false;
-      }
-
-      std::string line;
-      // Split up each line of the file.
-      while (getline(file_stream, line, '\n')) {
-        std::stringstream line_stream(line);
-
-        // Get a vector of numbers directly from a stream iterator.
-        std::istream_iterator<double> start(line_stream), end;
-        std::vector<double> numbers(start, end);
-        data.push_back(number);
-      }
-      return true;
+      std::cout << "Could not open file " << fileK << std::endl;
     }
 
-    void
-    propData ()
+    getline(file_stream, line, '\n');
+    std::stringstream line_stream(line);
+    std::istream_iterator<double> start(line_stream), end;
+    std::vector<double> numbers(start, end);
+    for (const auto x:numbers)
+      knots.push_back(x);
+  }
+
+  void
+  transfData ()
+  {
+    // clr transformation of prop_data in transf_data
+    double a = 1.0;
+    std::vector<double> temp;
+
+    for (auto& x:prop_data)
     {
-      // divide data in intervals, and count occurrences, and divide to
-      // obtain proportions
-      std::vector<double> v(nclasses,0.0);
-      unsigned int it = 0;
-      for(const auto& x:data)
+      // computing geometric mean
+      for (const auto& y:x)
+        a *= y;
+      a = pow(a, 1.0/nclasses);   // nclasses = x.size()
+
+      // clr transformation
+      for (const auto& y:x)
+        temp.push_back(log(y/a));
+
+      transf_data.push_back(temp);
+      temp.clear();
+    }
+  }
+
+  void
+  pacs()
+  {
+    for(const auto & it:transf_data)
+    dens(knots,xcp,it)
+  }
+
+  // NOTE: generalization for different data input
+
+  unsigned int
+  set_nclasses () const
+  {
+    // Adopting Sturge Formula
+    double k=0.0;
+    for(const auto& x:data)
+      k += 1.0 + ceil(log2(x.size()));
+    return (int) k/data.size();
+  }
+
+  void
+  set_intervals()
+  {
+    double l = (max-min)/nclasses;
+    unsigned int i = 0;
+    while (i < nclasses+1)
+      intervals.push_back(min + (i++)*l);
+  }
+
+  bool
+  loadData (const std::string& filepath)
+  {
+    std::ifstream file_stream(filepath, std::ios_base::in);
+    if (!file_stream) {
+      std::cout << "Could not open file " << filepath << std::endl;
+      return false;
+    }
+
+    std::string line;
+    // Split up each line of the file.
+    while (getline(file_stream, line, '\n')) {
+      std::stringstream line_stream(line);
+
+      // Get a vector of numbers directly from a stream iterator.
+      std::istream_iterator<double> start(line_stream), end;
+      std::vector<double> numbers(start, end);
+      data.push_back(number);
+    }
+    return true;
+  }
+
+  void
+  propData ()
+  {
+    // divide data in intervals, and count occurrences, and divide to
+    // obtain proportions
+    std::vector<double> v(nclasses,0.0);
+    unsigned int it = 0;
+    for(const auto& x:data)
+    {
+      for (const auto& y:x)
       {
-        for (const auto& y:x)
-        {
-          it = 0;
-          // while (y > intervals[++it]) {};           v[--it] ++;
-          while (y > intervals[it+1]) { it++; }
-          v[it] ++;
-        }
-        std::transform(v.begin(), v.end(), v.begin(),
-                 std::bind1st(std::divides<double>(),x.size()));
-        prop_data.push_back(v);
+        it = 0;
+        // while (y > intervals[++it]) {};           v[--it] ++;
+        while (y > intervals[it+1]) { it++; }
+        v[it] ++;
       }
-
-      //        how to deal with zero counts
-      // Because of count character of values in prop_data, also some zero values
-      // occurred that would make further processing by clr transformation impossible.
-      // For this reason, their imputation using a model-based procedure was performed
-      prop_data = coda::BM(prop_data);
+      std::transform(v.begin(), v.end(), v.begin(),
+               std::bind1st(std::divides<double>(),x.size()));
+      prop_data.push_back(v);
     }
 
-    void
-    transfData ()
-    {
-      // clr transformation of prop_data in transf_data
-      double a = 1.0;
-      for (auto& x:prop_data)
-      {
-        // computing geometric mean
-        for (const auto& y:x)
-          a *= y;
-        a = pow(a, 1.0/nclasses);   // nclasses = x.size()
+    //        how to deal with zero counts
+    // Because of count character of values in prop_data, also some zero values
+    // occurred that would make further processing by clr transformation impossible.
+    // For this reason, their imputation using a model-based procedure was performed
+    prop_data = coda::BM(prop_data);
+  }
 
-        // clr transformation
-        for (const auto& y:x)
-          transf_data.push_back(log(y/a));
-      }
-    }
+  void
+  antitData (std::vector<double>& v)
+  {
+    // anti clr transformation
+    double a = 0.0;
+    for (const auto& x:v)
+      a += exp(x);
+    for (auto& x:v)
+      x = exp(x)/a;
 
-    void
-    antitData (std::vector<double>& v)
-    {
-      // anti clr transformation
-      double a = 0.0;
-      for (const auto& x:v)
-        a += exp(x);
-      for (auto& x:v)
-        x = exp(x)/a;
-
-    }
-
+  }
 
 };
 
--- src/main.cpp
+++ src/main.cpp
@@ -19,57 +19,25 @@ int main(int argc, char* argv[]) {
     unsigned int l = filePara("l", 2);
     double alpha = filePara("alpha", 1.0);  // penalization parameter
     bool knots_given = filePara("knots_given", 0);
-    std::string line;
 
-    std::vector<double> knots;
-    std::vector<double> xcp;
-    std::vector<std::vector<double>> ycp;
+    Mother obj(k,l,alpha);
 
     // Read data
     const std::string fileD = commandline.follow("input/data", 1, "-d");
-    std::ifstream file_stream(fileD, std::ios_base::in);
-    if (!file_stream) {
-      std::cout << "Could not open file " << fileD << std::endl;
-    }
-
-    // Read xcp
-    getline(file_stream, line, '\n');
-    std::stringstream line_stream(line);
-    std::istream_iterator<double> start(line_stream), end;
-    std::vector<double> numbers(start, end);
-    for (const auto x:numbers)
-      xcp.push_back(x);
-
-    // Read ycp
-    while (getline(file_stream, line, '\n')) // Split up each line of the file.
-    {
-      std::stringstream line_stream(line);
-      // Get a vector of numbers directly from a stream iterator.
-      std::istream_iterator<double> start(line_stream), end;
-      std::vector<double> numbers(start, end);
-      ycp.push_back(numbers);
-    }
 
     // Read knots if knots_given
     if(knots_given)
     {
       const std::string fileK = commandline.follow("input/knots", 2, "-k", "--knots");
-      std::ifstream file_stream(fileK, std::ios_base::in);
-      if (!file_stream) {
-        std::cout << "Could not open file " << fileK << std::endl;
-      }
-
-      getline(file_stream, line, '\n');
-      std::stringstream line_stream(line);
-      std::istream_iterator<double> start(line_stream), end;
-      std::vector<double> numbers(start, end);
-      for (const auto x:numbers)
-        knots.push_back(x);
+      obj.readData(fileD,fileK);
     }
     else
     {
-
+      obj.createKnots();
+      obj.readData(fileD);
     }
+
+    obj.transfData();
 
     // Checking data are read correctly
     // for (const auto x:xcp)
@@ -82,25 +50,6 @@ int main(int argc, char* argv[]) {
     // }
 
     // Testing first row
-    std::vector<std::vector<double>> prop_data = coda::BM(ycp);
-    std::vector<std::vector<double>> transf_data;
-    std::vector<double> temp;
-    double a = 1.0;
-    for (auto& x:prop_data)
-    {
-      // computing geometric mean
-      for (const auto& y:x)
-        a *= y;
-      a = pow(a, 1.0/x.size());   // nclasses = x.size()
-
-      // clr transformation
-      for (const auto& y:x)
-        temp.push_back(log(y/a));
-
-      transf_data.push_back(temp);
-      temp.clear();
-    }
-
     // std::cout<<"prop_data: "<<std::endl;
     // for (const auto x:prop_data){
     //   for (const auto y:x)
