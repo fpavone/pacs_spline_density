9cbc02aaa5ca871b4c7de5781fd5c6ea9530b93b
fpavone <federicopavone01@gmail.com>
2018-05-22 14:44:25 +0200

    added progression bar in for loop

--- src/cuore.cpp
+++ src/cuore.cpp
@@ -7,6 +7,8 @@
 #include <iterator>
 #include <RcppEigen.h>
 #include <Rcpp.h>
+#include <thread>
+#include <chrono>
 
 
 using namespace Rcpp;
@@ -49,6 +51,8 @@ SEXP mymain(SEXP k_, SEXP l_, SEXP alpha_, SEXP data_, SEXP Xcp_, SEXP knots_, S
   Eigen::MatrixXd yvalueMat(nrow,numPoints);
   Eigen::MatrixXd yvalueMatClr(nrow,numPoints);
 
+  int barWidth = 70; // for bar progress plot
+
   for(std::size_t i = 0; i < nrow; i++)
   {
     obj.readData(data.row(i));
@@ -56,7 +60,19 @@ SEXP mymain(SEXP k_, SEXP l_, SEXP alpha_, SEXP data_, SEXP Xcp_, SEXP knots_, S
     obj.pacs(dens, bsplineMat.row(i));
     obj.plotData_parallel(dens, numPoints, bsplineMat.row(i), yvalueMat.row(i));
     obj.plotData_parallel_Clr(dens, numPoints, bsplineMat.row(i), yvalueMatClr.row(i));
+
+    std::cout << "[";
+    int pos = barWidth * (double)i/nrow;
+    for (int j = 0; j < barWidth; ++j) {
+      if (j < pos) std::cout << "=";
+      else if (j == pos) std::cout << ">";
+      else std::cout << " ";
+    }
+    std::cout << "] " << int((double)i/(nrow-1) * 100.0) << "%\r";
+    std::cout.flush();
+    //std::this_thread::sleep_for(std::chrono::seconds(1));
   }
+  std::cout << std::endl;
 
   List result = List::create(Named("bspline") = bsplineMat,
                              Named("Y") = yvalueMat,
