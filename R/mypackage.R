3a61bd767399eeeb3ebd1324b6bba0bd2b0c8a8e
fpavone <federicopavone01@gmail.com>
2018-05-24 11:02:18 +0200

    r script

---
+++ R/mypackage.R
@@ -0,0 +1,47 @@
+smoothingSplines <- function(k,l,alpha,data,xcp,knots,num_points = 100) 
+{
+  # Checking if data is a matrix
+  if ( !is.matrix(data) ) 
+  {
+    err <- simpleError("data must be a matrix type.")
+    stop(err)
+  }
+  
+  # Creating equispaced knots if not given
+  if( length(knots) == 1 )
+  {
+    u <- xcp[1]
+    v <- tail(xcp,n=1)
+    size <- knots
+    step <- (v - u)/(size-1)
+    knots_ <- seq(u,v, by = step)
+    obj <- .Call("mymain",as.integer(k),as.integer(l),alpha,data,xcp,knots_,as.integer(num_points))
+  }
+  else 
+   obj <- .Call("mymain",as.integer(k),as.integer(l),alpha,data,xcp,knots,as.integer(num_points))
+  
+  class(obj) <- "smoothSpl"
+  return(obj)
+}
+
+plot.smoothSpl <- function(obj, by = 1 , n = 10,...){
+  xx <- seq(obj$Xcp[1],tail(obj$Xcp,n=1),length.out = obj$NumPoints)
+  n <- min(n,dim(obj$Y)[1])
+  cols <- rainbow(min(n,30))
+  # Plotting in the clr space fitted curves
+  plot.default(xx,obj$Y_clr[1,],ylim=c(min(obj$Numbers),max(obj$Numbers)), 
+               type = "l", xlab = "", ylab = "")
+  title("Smoothing splines in clr-transformed space")
+  for(i in seq(1,n,by=by)){
+    lines(xx,obj$Y_clr[i,], col = cols[i%%length(cols)])
+  }
+  
+  # Plotting densities in orginal space
+  plot.default(xx,obj$Y[1,],ylim=c(min(obj$Y),max(obj$Y)), 
+               type = "l", xlab = "", ylab = "")
+  title("Density")
+  for(i in seq(1,n,by=by)){
+    lines(xx,obj$Y[i,], col = cols[i%%length(cols)])
+  }
+  
+}
