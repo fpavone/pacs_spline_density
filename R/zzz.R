f3017d5f2dfce9056001c3186ce5a7ac5e7367dd
fpavone <federicopavone01@gmail.com>
2018-07-24 14:19:49 +0200

    updated version

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
 .onAttach <- function(libname, pkgname)
-  packageStartupMessage("splineDensity 0.0.0.23 loaded\nCopyright Noi 2018")
+  packageStartupMessage("splineDensity 0.1.23 loaded\nCopyright Noi 2018")
 
 .onUnload <- function(libpath)
   library.dynam.unload("splineDensity",  libpath)
