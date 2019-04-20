8c58336b6367c43a64ef3a03a473f0a6b19aeb50
fpavone <federicopavone01@gmail.com>
2019-04-20 14:20:49 +0200

    changed T and F to TRUE and FALSE in R scripts

--- tests/test_iris.r
+++ tests/test_iris.r
@@ -4,10 +4,10 @@ SepalLengthCm <- iris$Sepal.Length
 Species <- iris$Species
  
 iris1 <- SepalLengthCm[iris$Species==levels(iris$Species)[1]]
-h1 <- hist(iris1, nclass = 12, plot = F)
+h1 <- hist(iris1, nclass = 12, plot = FALSE)
 
 midx1 <- h1$mids
-midy1 <- matrix(h1$density, nrow=1, ncol = length(h1$density), byrow=T)
+midy1 <- matrix(h1$density, nrow=1, ncol = length(h1$density), byrow=TRUE)
 knots <- 7 
 sol1 <- smoothSplines(k=3,l=2,alpha=1000,midy1,midx1,knots)
 plot(sol1)
