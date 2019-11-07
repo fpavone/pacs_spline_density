8eddf881b273a4188cdd1c3046097a923fdbea50
gianluca zeni <gianluca.zeni@polimi.it>
2019-11-07 18:28:19 +0100

    Update splineDensity.plot.R

--- R/splineDensity.plot.R
+++ R/splineDensity.plot.R
@@ -1,7 +1,7 @@
 #' @export
-plot.smoothSpl <- function(obj, by = 1 , n = 10, index = NULL, ...){
-  xx <- seq(obj$Xcp[1],tail(obj$Xcp,n=1),length.out = obj$NumPoints)
-  n <- min(n,dim(obj$Y)[1])
+plot.smoothSpl <- function(x, by = 1 , n = 10, index = NULL, ...){
+  xx <- seq(x$Xcp[1],tail(x$Xcp,n=1),length.out = x$NumPoints)
+  n <- min(n,dim(x$Y)[1])
   cols <- rainbow(min(n,30))
   if(is.null(index)) {
     whitch <- seq(1,n,by=by)
@@ -9,19 +9,19 @@ plot.smoothSpl <- function(obj, by = 1 , n = 10, index = NULL, ...){
     whitch <- index
   }
   # Plotting in the clr space fitted curves
-  plot.default(xx,obj$Y_clr[1,],ylim=c(min(obj$Y_clr[whitch,]),max(obj$Y_clr[whitch,])),
+  plot.default(xx,x$Y_clr[1,],ylim=c(min(x$Y_clr[whitch,]),max(x$Y_clr[whitch,])),
                type = "n", xlab = "", ylab = "")
   title("Smoothing splines in clr-transformed space")
   for(i in whitch){
-    lines(xx,obj$Y_clr[i,], col = cols[i%%length(cols) + 1])
+    lines(xx,x$Y_clr[i,], col = cols[i%%length(cols) + 1])
   }
 
   # Plotting densities in orginal space
   
-  plot.default(xx,obj$Y[1,],ylim=c(min(obj$Y[whitch,]),max(obj$Y[whitch,])),
+  plot.default(xx,x$Y[1,],ylim=c(min(x$Y[whitch,]),max(x$Y[whitch,])),
                type = "n", xlab = "", ylab = "")
   title("Density")
   for(i in whitch){
-    lines(xx,obj$Y[i,], col = cols[i%%length(cols) + 1])
+    lines(xx,x$Y[i,], col = cols[i%%length(cols) + 1])
   }
 }
